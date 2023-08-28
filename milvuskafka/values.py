dd2cbe777777a9b135950f202714ea98b223766a
Filip Haltmayer <filip.haltmayer@zilliz.com>
2023-08-28 13:21:02 -0700

    Work with zilliz and confluent cloud
    Signed-off-by: Filip Haltmayer <filip.haltmayer@zilliz.com>

--- milvuskafka/values.py
+++ milvuskafka/values.py
@@ -1,14 +1,23 @@
 KAKFA_POLL_TIMEOUT = 1
+KAFKA_REPLICATION_FACTOR = 1 # 3 for cloud
 KAFKA_TOPICS = {
     "SEARCH_PRODUCER_TOPIC": "SearchResponse",
     "SEARCH_CONSUMER_TOPIC": "SearchRequest",
     "INSERT_CONSUMER_TOPIC": "InsertRequest"
 }
+KAFKA_DEFAULT_CONFIGS = {
+    "bootstrap.servers": "localhost:9094",
+}
+# KAFKA_DEFAULT_CONFIGS = {
+#     "bootstrap.servers": "redacted",
+#     "security.protocol": "SASL_SSL",
+#     "sasl.mechanisms": "PLAIN",
+#     "sasl.username": "redacted",
+#     "sasl.password": "redacted"
+# }
 
 MILVUS_COLLECTION = "kafkalection"
 MILVUS_DIM = 3
 
-
 MILVUS_URI = "http://localhost:19530"
 MILVUS_TOKEN = ""
-KAFKA_ADDRESS = "localhost:9094"
